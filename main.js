b6fcb125926f6b79709c84e5765fda0060d007ec
Miguel Torrico <mickimaick18@gmail.com>
2023-12-15 17:26:54 -0400

    Prueba sonido en boton y javascript en body

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert("hola mundo");
+function playSonidoPom () {
+    document.querySelector('#sonido_tecla_pom').play();
+}
+
+document.querySelector(".tecla_pom").onclick = playSonidoPom;
+//prueba para sonido primer boton
+
+
