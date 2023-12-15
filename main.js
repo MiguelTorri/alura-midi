d63e1da833d6315637554155892c57cab9a98d96
Miguel Torrico <mickimaick18@gmail.com>
2023-12-15 18:15:50 -0400

    Optimizando el codigo mas botones

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-function playSonidoPom () {
-    document.querySelector('#sonido_tecla_pom').play();
+function playSonido (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector(".tecla_pom").onclick = playSonidoPom;
-//prueba para sonido primer boton
+//optimizando el codigo 
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
 
 
+while (contador < 9) {
+    //constante que selecciona el boton 1° clase 
+    const tecla = listaDeTeclas[contador];
+    //constante que selecciona la 2° clase
+    const instrumento = tecla.classList[1];
+    //constante con temple string para elegir el audio correspondiente
+    const idAudio = `#sonido_${instrumento}`
+
+    //uso de funciones anonimas la invoca pero no la ejecuta 
+    tecla.onclick = function(){
+        playSonido(idAudio);
+    }
+    contador++;
+}
