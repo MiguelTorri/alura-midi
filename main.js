8032202313d2511e0f5b9ede78c299bb8d4a7663
Miguel Torrico <mickimaick18@gmail.com>
2023-12-18 09:49:15 -0400

    alternativa for

--- main.js
+++ main.js
@@ -5,20 +5,26 @@ function playSonido (idElementoAudio) {
 //optimizando el codigo 
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-
-while (contador < 9) {
-    //constante que selecciona el boton 1° clase 
+for(let contador = 0;contador < listaDeTeclas.length;contador++) {
+     
     const tecla = listaDeTeclas[contador];
-    //constante que selecciona la 2° clase
     const instrumento = tecla.classList[1];
-    //constante con temple string para elegir el audio correspondiente
     const idAudio = `#sonido_${instrumento}`
+    tecla.onclick = function(){
+        playSonido(idAudio);
+    }
+}
 
-    //uso de funciones anonimas la invoca pero no la ejecuta 
+
+/* alternativa anterior
+while (contador < 9) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#sonido_${instrumento}`
     tecla.onclick = function(){
         playSonido(idAudio);
     }
     contador++;
 }
+*/
